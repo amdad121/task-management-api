3a181782de41414a4ab09f1eee11c52ff1db996e
Amdadul Haq <amdadulhaq781@gmail.com>
2023-07-20 16:32:13 +0600

    auth route added

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/login', [AuthController::class, 'login'])->middleware('guest');
+Route::post('/register', [AuthController::class, 'register'])->middleware('guest');
+
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
